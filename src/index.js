2eef66655f5c092c4488c55474d724a755a44ef6
zewdu erkyhun <zewdu444@gmail.com>
2023-01-18 12:13:36 +0300

    Add style for todolist

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
+
+const tasks = [{
+  description: 'Wash clothes',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'Complete project',
+  completed: false,
+  index: 2,
+},
+
+{
+  description: 'Read books',
+  completed: false,
+  index: 3,
+},
+];
