22f527f89aed0a79f124ba45c1f476fcd257d40f
zewdu erkyhun <zewdu444@gmail.com>
2023-01-18 13:01:44 +0300

    Add javascript  to display list dynamically

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
 
+const todoList = document.querySelector('.todolist');
+
 const tasks = [{
   description: 'Wash clothes',
   completed: false,
@@ -19,3 +21,15 @@ const tasks = [{
   index: 3,
 },
 ];
+
+const dynamicDisplay = () => {
+  tasks.forEach((task) => {
+    todoList.innerHTML += `<li>
+    <input type="checkbox" ${task.index.completed ? 'checked' : 'unchecked'} />
+    <p>${task.description}</p>
+    <i class="fa fa-ellipsis-v todolisticon" aria-hidden="true"></i>
+   </li>
+   <hr />`;
+  });
+};
+dynamicDisplay();
